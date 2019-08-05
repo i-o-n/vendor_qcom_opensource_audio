f6a610121084f1fe8eb7c18fa632b23677328067
Akhil Narang <akhilnarang.1999@gmail.com>
2019-08-06 01:02:26 +0530

    AudioPolicyManager: fix build after r46 merge
    Commit https://github.com/i-o-n/frameworks_av/commit/97fa3267af541d6c5fe0cafb53592056b4c10c2e changed mPolicyMix to not be a pointer
    
    Change-Id: Iff486867fdf0749c34f4f9b3e29cb45dc2721af9
    Signed-off-by: Akhil Narang <akhilnarang.1999@gmail.com>

--- policy_hal/AudioPolicyManager.cpp
+++ policy_hal/AudioPolicyManager.cpp
@@ -46,6 +46,7 @@
 #include <soundtrigger/SoundTrigger.h>
 #include "AudioPolicyManager.h"
 #include <policy.h>
+#include <AudioPolicyMix.h>
 
 namespace android {
 /*audio policy: workaround for truncated touch sounds*/
@@ -2224,6 +2225,8 @@ status_t AudioPolicyManagerCustom::startInput(audio_io_handle_t input,
     // Routing?
     mInputRoutes.incRouteActivity(session);
 
+    sp<AudioPolicyMix> policyMix = inputDesc->mPolicyMix.promote();
+
     if (audioSession->activeCount() == 1 || mInputRoutes.getAndClearRouteChanged(session)) {
         // indicate active capture to sound trigger service if starting capture from a mic on
         // primary HW module
@@ -2239,8 +2242,8 @@ status_t AudioPolicyManagerCustom::startInput(audio_io_handle_t input,
         if (inputDesc->getAudioSessionCount(true/*activeOnly*/) == 1) {
             // if input maps to a dynamic policy with an activity listener, notify of state change
             if ((inputDesc->mPolicyMix != NULL)
-                    && ((inputDesc->mPolicyMix->mCbFlags & AudioMix::kCbFlagNotifyActivity) != 0)) {
-                mpClientInterface->onDynamicPolicyMixStateUpdate(inputDesc->mPolicyMix->mDeviceAddress,
+                    && ((policyMix->mCbFlags & AudioMix::kCbFlagNotifyActivity) != 0)) {
+                mpClientInterface->onDynamicPolicyMixStateUpdate(policyMix->mDeviceAddress,
                         MIX_STATE_MIXING);
             }
 
@@ -2261,8 +2264,8 @@ status_t AudioPolicyManagerCustom::startInput(audio_io_handle_t input,
                 String8 address = String8("");
                 if (inputDesc->mPolicyMix == NULL) {
                     address = String8("0");
-                } else if (inputDesc->mPolicyMix->mMixType == MIX_TYPE_PLAYERS) {
-                    address = inputDesc->mPolicyMix->mDeviceAddress;
+                } else if (policyMix->mMixType == MIX_TYPE_PLAYERS) {
+                    address = policyMix->mDeviceAddress;
                 }
                 if (address != "") {
                     setDeviceConnectionStateInt(AUDIO_DEVICE_OUT_REMOTE_SUBMIX,
